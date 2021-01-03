6170532b7e3091f4bb5247b78ec74594bcbe9aec
Pedal <jmb181@humboldt.edu>
2021-01-02 22:52:12 -0800

    connected to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-// import the Sequelize constructor from the library
+// import the Sequelize class constructor from the library
 const Sequelize = require('sequelize');
 // doesn't have to be saved to variable. Gives access to .env file. 
 require('dotenv').config();
@@ -6,12 +6,11 @@ require('dotenv').config();
 // initialize sequelize
 let sequelize;
 
-//pass in MySQL information for username and password
 // Sets up sequelize for JAWSDB or Localhost
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
   } else {
-    // use .env variables to keep private
+    // If using local db - use .env variables to keep private
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
